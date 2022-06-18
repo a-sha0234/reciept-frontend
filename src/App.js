a390077f14132e0b3012526b49549bc6381d442a
a-sha0234 <asharma_31@hotmail.com>
2022-06-18 22:49:18 +0100

    add react routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { react } from "react";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-    
+      {" "}
+      <BrowserRouter>
+        {" "}
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
