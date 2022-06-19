bf65ddac5ce99f5d32573f8384531d39bbab6553
a-sha0234 <asharma_31@hotmail.com>
2022-06-19 02:41:26 +0100

    create async functions for adding and reading receipts

--- src/api/receipts.js
+++ src/api/receipts.js
@@ -2,8 +2,23 @@ import axios from "axios";
 import instance from "../axios";
 
 async function fetchAllReceipts(setter) {
-  const getReceipts = await instance.get("/");
-  setter(getReceipts.data);
+  // function to retrive all receipts
+  try {
+    const getReceipts = await instance.get("/");
+    setter(getReceipts.data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-export { fetchAllReceipts };
+async function addReceipt(data) {
+  // add receipts to database
+  try {
+    const addReceipt = await instance.post("/createReceipt", data);
+    console.log(addReceipt);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export { fetchAllReceipts, addReceipt };
