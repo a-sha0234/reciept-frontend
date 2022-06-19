ad243c3020014240c5287af0b311e197f32e35f6
a-sha0234 <asharma_31@hotmail.com>
2022-06-19 02:39:21 +0100

    call api functions for creating and reading receipts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,33 @@
-export default function Home() {
+import { useEffect, useState } from "react";
+import Form from "../components/Form";
+import { fetchAllReceipts, addReceipt } from "../api/receipts";
+import { v4 as uuidv4 } from "uuid";
 
-    
-  return <div></div>;
+export default function Home() {
+  const [userFormData, setUserFormData] = useState({}); // stores users form data
+
+  const [storeReceipts, setStoreReceipts] = useState(); // stores all receipts data
+
+  useEffect(() => {
+    // api call to fetch all receipts
+    fetchAllReceipts(setStoreReceipts);
+  }, []);
+
+  useEffect(() => {
+    // api call to add receipt to database
+    if (Object.keys(userFormData).length !== 0) {
+      let formObject = userFormData;
+      formObject.id = uuidv4();
+      console.log(formObject);
+      addReceipt(formObject);
+    }
+  }, [userFormData]);
+
+  console.log(storeReceipts);
+
+  return (
+    <div>
+      <Form setUserFormData={setUserFormData} />
+    </div>
+  );
 }
