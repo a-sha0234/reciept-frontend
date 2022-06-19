592c317501043c9a2ef50fca029c86c73a4e4d85
a-sha0234 <asharma_31@hotmail.com>
2022-06-19 21:23:14 +0100

    create table row form for updating any row

---
+++ src/components/EditTableRow.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { updateReceipt } from "../api/receipts";
+
+export default function EditTableRow(props) {
+  const [formData, setFormData] = useState({
+    name: "",
+    location: "",
+    cost: "",
+  });
+
+  function handleChange(e) {
+    // changes formData with every keystroke
+    const { name, value } = e.target;
+    return setFormData((prev) => {
+      return {
+        ...prev, // return previous data
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(e) {
+    updateReceipt(props.storeUpdateId, formData);
+  }
+
+  console.log(formData.name, formData.location);
+  return (
+    <form>
+      <label>Name</label>
+      <input
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        required
+      ></input>
+
+      {/*  */}
+      <label>Location</label>
+      <input
+        name="location"
+        value={formData.location}
+        onChange={handleChange}
+        required
+      ></input>
+      {/*  */}
+      <label>Price</label>
+      <input
+        name="cost"
+        value={formData.cost}
+        onChange={handleChange}
+        required
+      ></input>
+      <button onClick={handleSubmit}>Apply Changes </button>
+    </form>
+  );
+}
